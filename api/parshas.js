eb57bf383b22d739e1f1fce47b3d164693a166eb
Reuvain Neufeld <ryn4310@gmail.com>
2021-08-31 17:35:47 -0400

    ??

--- api/parshas.js
+++ api/parshas.js
@@ -67,14 +67,14 @@ router.get("/advancedVorts", (req, res, next) => {
 module.exports = router;
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+    // app.use(express.static('client/build'));
 
     // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-    });
+    // const path = require('path');
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+    // });
     app.listen(process.env.PORT)
 }
-app.listen(port);
+app.listen(port)
 app.use('/', router);
